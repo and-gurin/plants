2941db91f6bf57737964d2b0b460041b91e8f519
and-gurin <and.gurin@gmail.com>
2023-02-08 22:54:05 +0100

    fix: resolve mistakes in accordion

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,43 +1,17 @@
 const addAccordionClickHandler = () => {
-    let accordionLabel = document.querySelectorAll('.accordion__container');
-    console.log(accordionLabel)
+    let accordionLabel = document.querySelectorAll('.accordion__label');
 
     accordionLabel.forEach(label => {
-        label.addEventListener('click',(e)=>{
-          let openContent = document.querySelectorAll('.accordion__content')
-          let openItems = document.querySelectorAll('.accordion_active')
-          console.log(openItems)
-          // openContent.forEach(content=>{
-          //   content.style.display = "none";
-          // })
-          // openItems.forEach(openItem=>{
-          //   openItem.classList.remove('accordion_active')
-          // })
-          let content = label.lastElementChild;
-          let openItem = document.getElementsByClassName('accordion_active');
-          openItem.forEach(item=>{
-            if (item) {
-            e.target.classList.remove('accordion_active');
-            content.style.display = "none";
-          }
-          if (e.target.classList.contains('accordion__label')) {
-            
-              e.target.classList.toggle('accordion_active')
-            } 
-          })
-          
-          
-           
-      
-        if (content.style.display === "block") {
-          content.style.display = "none";
-        } else {
-          content.style.display = "block";
-        }
-        })
+        label.addEventListener('click', accordionLabelHandler)
     })
-        
-}
 
+    function accordionLabelHandler () {
+      accordionLabel.forEach(label=>{
+        label.classList.remove('accordion_active')
+      })
+      this.classList.toggle('accordion_active')
+      
+    }     
+}
 
 addAccordionClickHandler()
